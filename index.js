c7fb4dcefcf312efc77dfe8baa9035c299e6688b
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-07-31 19:16:50 +0800

    Fixed deployment settings

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
-const morganBody = require('morgan-body');
+//const morganBody = require('morgan-body');
 const mongoose = require('mongoose');
 
 const app = express();
 const dev = app.get('env') !== 'production';
 const PORT = process.env.PORT || 3001;
 
-if (!dev) {
-  app.disable('x-powered-by');
-  app.use(morgan('common'));
-
-  app.use(express.static(path.resolve(__dirname, 'client/build')));
-} else {
-  app.use(morgan('dev'));
-  //morganBody(app);
-}
-
 app.enable('trust proxy');
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/whenmeet', { useNewUrlParser: true });
+app.use(express.static(path.resolve(__dirname, 'client/build')));
+
+if (!dev) {
+  app.disable('x-powered-by');
+  app.use(morgan('common'));
+
+  mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true, dbName: 'whenmeet' });
+
+} else {
+  app.use(morgan('dev'));
+  //morganBody(app);
+  mongoose.connect('mongodb://127.0.0.1:27017/whenmeet', { useNewUrlParser: true });
+}
+
 const connection = mongoose.connection;
 
 connection.once('open', () => {
