847f11088dfbf06bb0bc1e25f688ded1277394fa
Nanosync <10258208+Nanosync@users.noreply.github.com>
2019-07-31 21:43:51 +0800

    More restrictive cors

--- index.js
+++ index.js
@@ -9,23 +9,19 @@ const app = express();
 const dev = app.get('env') !== 'production';
 const PORT = process.env.PORT || 3001;
 
-app.enable('trust proxy');
+let whitelist = ['http://whencanmeet.herokuapp.com', 'https://whencanmeet.herokuapp.com', 'http://whencanmeet.benedictpak.com']
 
-app.use(cors());
-app.use(express.json());
-
-app.use(express.static(path.resolve(__dirname, 'client/build')));
-
-if (!dev) {
-  app.disable('x-powered-by');
-  app.use(morgan('common'));
-
-  mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true, dbName: 'whenmeet' });
-
-} else {
+if (dev) {
+  whitelist.push('http://localhost:3000');
   app.use(morgan('dev'));
   //morganBody(app);
   mongoose.connect('mongodb://127.0.0.1:27017/whenmeet', { useNewUrlParser: true });
+} else {
+  app.disable('x-powered-by');
+  app.use(morgan('common'));
+
+  app.use(express.static(path.resolve(__dirname, 'client/build')));
+  mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true, dbName: 'whenmeet' });
 }
 
 const connection = mongoose.connection;
@@ -33,6 +29,21 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB connection established successfully');
 });
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.enable('trust proxy');
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 // Simple middleware for IP logging
 app.use(function(req, res, next) {
